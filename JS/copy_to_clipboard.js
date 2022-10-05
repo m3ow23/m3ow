f27779f0e98bf3303e454630cb58cbc182f07e47
m3ow23 <nathanaelmemis@gmail.com>
2022-10-05 23:26:53 +0800

    test

--- JS/copy_to_clipboard.js
+++ JS/copy_to_clipboard.js
@@ -1,6 +1,9 @@
 function generateHash() {
     var userInput = document.getElementById("userInput").value; 
 
+    if (!navigator.clipboard)
+        alert('Clipboard API is not available');
+
     if (userInput != "") {
         navigator.clipboard.writeText(m3ow_hash(userInput));
 
