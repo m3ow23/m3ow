47f5211e5a2682d954d0d07946f9299a8263f7bf
m3ow23 <nathanaelmemis@gmail.com>
2022-10-05 23:34:39 +0800

    Update copy_to_clipboard.js

--- JS/copy_to_clipboard.js
+++ JS/copy_to_clipboard.js
@@ -1,18 +1,17 @@
 function generateHash() {
     var userInput = document.getElementById("userInput"); 
 
-    if (!navigator.clipboard)
+    if (userInput == "") {
+        alert("Please provide an input!");
+        return;
+    }
+    if (!navigator.clipboard) {
         alert('Clipboard API is not available');
-
-    if (userInput != "") {
-        userInput.select();
-        userInput.setSelectionRange(0, 99999);
-        navigator.clipboard.writeText(m3ow_hash(userInput.value));
-
-        alert("Hash is copied to clipboard!");
         return;
     }
 
-    alert("Please provide an input!");
+    userInput.select();
+    userInput.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(m3ow_hash(userInput.value));
     return;
 }
