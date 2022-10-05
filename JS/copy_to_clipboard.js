92819cf0d4d8bc900a67c60bd6fe547f18fc9c52
m3ow23 <nathanaelmemis@gmail.com>
2022-10-05 23:01:44 +0800

    Update copy_to_clipboard.js

--- JS/copy_to_clipboard.js
+++ JS/copy_to_clipboard.js
@@ -4,7 +4,7 @@ function generateHash() {
     if (userInput != "") {
         navigator.clipboard.writeText(m3ow_hash(userInput));
 
-        alert(userInput);
+        alert(m3ow_hash(userInput));
         return;
     }
 
